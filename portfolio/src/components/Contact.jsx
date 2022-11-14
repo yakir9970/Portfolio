5425cb25f1540ed096ff4d73bba8233a029b728b
yakir9970 <yakirafeka@gmail.com>
2022-11-14 16:48:46 +0200

    Skill, work & contact form

---
+++ portfolio/src/components/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div name='contact' className='w-full md:h-screen bg-[#0a192f] text-[#ccd6f6] flex justify-center items-center p-4'>
+      <form action="" className='flex flex-col max-w-[600px] w-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#ff9a63]'>Contact</p>
+            <p className='py-4'>// Submit the form below or send an email - Yakir.Travish1@gmail.com</p>
+        </div>
+
+        <input className='p-2 bg-[#ccd6f6] font-bold text-black' type="text" placeholder='Name' name='name' />
+        <input className='my-4 p-2 bg-[#ccd6f6] font-bold text-black' type="email" placeholder='Email' name='email' />
+        <textarea className='p-2 bg-[#ccd6f6] font-bold text-black' placeholder='Message' name="message" rows="10"></textarea>
+
+        <button className='text-[#ccd6f6] border-[#ccd6f6] border-2 px-6 py-3 my-8 mx-auto flex items-center text-sm md:text-lg font-bold hover:bg-[#ff9a63] hover:text-[#0a192f] hover:border-[#0a192f]'>Send</button>
+      </form>
+    </div>
+  )
+}
+
+export default Contact
---
+++ portfolio/src/components/Work.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import test from '../assets/test.png'
+
+const Work = () => {
+  return (
+    <div name='work' className='w-full md:h-screen bg-[#0a192f] text-[#ccd6f6]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#ff9a63]'>Work</p>
+            <p className='py-4'>// Check out some of my recent work</p>
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+
+            <div style={{backgroundImage: `url(${test})`}} className='relative shadow-lg shadow-[#535c74] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/*hover effects */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                    test application name
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="/">
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-2 border-black bg-transparent text-[#ccd6f6] font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href="/">
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div style={{backgroundImage: `url(${test})`}} className='relative shadow-lg shadow-[#535c74] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/*hover effects */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                    test application name
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="/">
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href="/">
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div style={{backgroundImage: `url(${test})`}} className='relative shadow-lg shadow-[#535c74] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/*hover effects */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                    test application name
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="/">
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href="/">
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div style={{backgroundImage: `url(${test})`}} className='relative shadow-lg shadow-[#535c74] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                {/*hover effects */}
+                <div className='opacity-0 group-hover:opacity-100'>
+                    <span className='text-2xl font-bold text-white tracking-wider'>
+                    test application name
+                    </span>
+                    <div className='pt-8 text-center'>
+                        <a href="/">
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Demo</button>
+                        </a>
+                        <a href="/">
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-2 border-black bg-white text-[#ccd6f6] font-bold text-lg'>Code</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Work
