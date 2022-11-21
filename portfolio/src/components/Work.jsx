c7bffe833f1f00b09c9cccea323f95066f36be21
yakir9970 <yakirafeka@gmail.com>
2022-11-21 13:41:25 +0200

    finishing

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -17,72 +17,72 @@ const Work = () => {
         <div className='grid md:grid-cols-2 gap-4'>
 
             <div className='relative shadow-lg shadow-[#535c74] dark:shadow-[#374151] group/item container rounded-md flex justify-center items-center mx-auto content-div'>
-                <img src={Where} alt="test" className='absolute top-0 right-0 group-hover/item:blur-sm w-full h-full object-cover rounded-md' />
+                <img src={Where} alt="test" className='absolute top-0 right-0 group-hover/item:blur-[8px] w-full h-full object-cover rounded-md' />
                 {/*hover effects */}
                 <div className='invisible group-hover/item:visible md:group-hover:flex z-10'>
-                    <span className='text-3xl font-extrabold text-[#0a192f] tracking-wider'>
+                    <span className='text-3xl font-extrabold text-black tracking-wider'>
                     Where is it? - Share places
                     </span>
                     <div className='pt-8 text-center'>
                         <a target="_blank" rel="noreferrer" href="https://where-is-it-mern.web.app/">
-                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Demo</button>
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Demo</button>
                         </a>
                         <a target="_blank" rel="noreferrer" href="https://github.com/yakir9970/MERN-Share-Places">
-                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Code</button>
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Code</button>
                         </a>
                     </div>
                 </div>
             </div>
 
             <div className='relative shadow-lg shadow-[#535c74] dark:shadow-[#374151] group/item container rounded-md flex justify-center items-center mx-auto content-div'>
-                <img src={proshop} alt="test" className='absolute top-0 right-0 group-hover/item:blur-sm w-full h-full object-cover rounded-md' />
+                <img src={proshop} alt="test" className='absolute top-0 right-0 group-hover/item:blur-[8px] w-full h-full object-cover rounded-md' />
                 {/*hover effects */}
                 <div className='invisible group-hover/item:visible md:group-hover:flex z-10'>
-                    <span className='text-3xl font-extrabold text-[#0a192f] tracking-wider'>
+                    <span className='text-3xl font-extrabold text-black tracking-wider'>
                     ProShop - Tech store
                     </span>
                     <div className='pt-8 text-center'>
                         <a target="_blank" rel="noreferrer" href="https://proshop-travish.herokuapp.com/">
-                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Demo</button>
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Demo</button>
                         </a>
                         <a target="_blank" rel="noreferrer" href="https://github.com/yakir9970/MERN-eCommerce">
-                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Code</button>
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Code</button>
                         </a>
                     </div>
                 </div>
             </div>
 
             <div className='relative shadow-lg shadow-[#535c74] dark:shadow-[#374151] group/item container rounded-md flex justify-center items-center mx-auto content-div'>
-                <img src={todo} alt="test" className='absolute top-0 right-0 group-hover/item:blur-sm w-full h-full object-cover rounded-md' />
+                <img src={todo} alt="test" className='absolute top-0 right-0 group-hover/item:blur-[8px] w-full h-full object-cover rounded-md' />
                 {/*hover effects */}
                 <div className='invisible group-hover/item:visible md:group-hover:flex z-10'>
-                    <span className='text-3xl font-extrabold text-[#0a192f] tracking-wider'>
-                    Team to-do List
+                    <span className='text-3xl font-extrabold text-black tracking-wider'>
+                    Team to-do list
                     </span>
                     <div className='pt-8 text-center'>
                         <a target="_blank" rel="noreferrer" href="https://github.com/yakir9970/todo">
-                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Demo</button>
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Demo</button>
                         </a>
                         <a target="_blank" rel="noreferrer" href="https://todo-list-yakir.herokuapp.com">
-                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Code</button>
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Code</button>
                         </a>
                     </div>
                 </div>
             </div>
 
             <div className='relative shadow-lg shadow-[#535c74] dark:shadow-[#374151] group/item container rounded-md flex justify-center items-center mx-auto content-div'>
-                <img src={blog} alt="test" className='absolute top-0 right-0 group-hover/item:blur-sm w-full h-full object-cover rounded-md' />
+                <img src={blog} alt="test" className='absolute top-0 right-0 group-hover/item:blur-[8px] w-full h-full object-cover rounded-md' />
                 {/*hover effects */}
                 <div className='invisible group-hover/item:visible md:group-hover:flex z-10'>
-                    <span className='text-3xl font-extrabold text-[#0a192f] tracking-wider'>
+                    <span className='text-3xl font-extrabold text-black tracking-wider'>
                     Personal heroes blog
                     </span>
                     <div className='pt-8 text-center'>
                         <a target="_blank" rel="noreferrer" href="https://github.com/yakir9970/MEVN1">
-                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Demo</button>
+                            <button className='absolute bottom-0 right-0 text-center w-1/2 py-3 border-t-2 border-l-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Demo</button>
                         </a>
                         <a target="_blank" rel="noreferrer" href="https://vast-dawn-98978.herokuapp.com/">
-                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-[#0a192f] font-bold text-xl'>Code</button>
+                            <button className='absolute bottom-0 left-0 text-center w-1/2 py-3 border-t-2 border-r-[1px] border-[#535c74]/80 hover:bg-[#0a192f]/95 rounded-md hover:text-[#ff9a63] bg-transparent text-black dark:hover:text-[#f9fafb] dark:hover:bg-[#3429aa]/90 font-bold text-xl'>Code</button>
                         </a>
                     </div>
                 </div>
